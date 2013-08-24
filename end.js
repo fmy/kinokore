359ed59a59d8cfa25975abbf7e247ce7af5950f5
shishi999 <seatod.noonoe@gmail.com>
2013-08-25 04:00:21 +0900

    no message

--- end.js
+++ end.js
@@ -1,13 +1,32 @@
+//============================================//
+//                   end.js                   //
+//                Create EndScene
+//                By. S.Yamaguchi
+// ===========================================//
+
+//ゲームが終わったあとの画面とか処理とか
+//！仮実装！//
 function startEndScene() {
     var scene = new Scene();
-    var GameOverImage = new Sprite(PLAYER_SIZE, PLAYER_SIZE);
+    var GameOverImage = new Sprite(189, 97);
     GameOverImage.image = game.assets[TEXNAME_END];
-    GameOverImage.width = PLAYER_SIZE;
-    GameOverImage.height = PLAYER_SIZE + 500;
+    GameOverImage.width = 189;
+    GameOverImage.height = 97;
     GameOverImage.flame = 0;
+    GameOverImage.x = (SCREEN_WIDTH - 189) / 2;
+    GameOverImage.y = 150;
+    GameOverImage.opacity = 0.0;
+    
     scene.addChild(GameOverImage);
     scene.addEventListener('touchstart', function(e){
          game.replaceScene(startTitleScene());
+    });
+    GameOverImage.addEventListener('enterframe', function(e){
+        if(GameOverImage.opacity <= 1.0)
+            {
+                GameOverImage.y += 0.5;
+                GameOverImage.opacity += 0.1
+            }   
     });
     return scene;
 }
