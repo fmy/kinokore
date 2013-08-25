255b10d426d41ce6a8578446f47c9630ff7a0821
Fumiya KARASAWA <krswfmy@gmail.com>
2013-08-25 10:39:24 +0900

    dokukinoko

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ window.onload = function() {
     game.rootScene.backgroundColor = "white";
     game.preload([TEXNAME_PLAYER, TEXNAME_BACKGROUND, TEXNAME_TREE, TEXNAME_ROAD,
         TEXNAME_NUMBER, TEXNAME_START, TEXNAME_END, TEXNAME_TITLE, TEXNAME_TITLESTRING,
-        TEXNAME_DAMAGE]);
+        TEXNAME_DAMAGE, TEXNAME_BENITENGU]);
     for (var i = 0; i < DATA.length; i++) {
         game.preload(DATA[i].img);
     }
